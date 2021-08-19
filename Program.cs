bcbaa96b4e871eb5866782addcd369a44ce29aff
Sam Carbine <scarbine@me.com>
2021-08-19 15:44:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rock_papper_sissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
