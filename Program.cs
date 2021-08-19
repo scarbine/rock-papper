3b9994ac34df917b0aa086973f183d5dfd67fa76
Sam Carbine <scarbine@me.com>
2021-08-19 16:09:49 -0500

    building base functions and vars

--- Program.cs
+++ Program.cs
@@ -5,8 +5,51 @@ namespace rock_papper_sissors
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+            // Vars
+
+            void Rock()
+            {
+             Console.WriteLine("Rock");
+            }
+            void Papper()
+            {
+             Console.WriteLine("Papper");
+            }
+            void Scissors()
+            {
+             Console.WriteLine("Scissors");
+            }
+
+            // Function Calls
+            Scoreboard(); 
+            Rock();
+            Papper();
+            Scissors();
+
+
+
+            // Display Header for the Game
+
+            void Scoreboard()
+            {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("|  Player: 0    |   Computer: 0");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("What would you like to throw?");
+            Console.WriteLine("1) Rock");
+            Console.WriteLine("2) Papper");
+            Console.WriteLine("3) Scissors");
+            Console.WriteLine("-------------------------------");
+            };
+
+            // Accept User input
+
+
+
+
         }
     }
 }
