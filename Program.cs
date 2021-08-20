82ee519b000c819a8f7a9d954e9ccb75054eaa60
Sam Carbine <scarbine@me.com>
2021-08-20 11:28:55 -0500

    added randomly generated answer for computer and displayed the results

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace rock_papper_sissors
 
             // Vars
 
+            int HumanScore = 0;
+            int ComputerScore = 0;
+            string ComputerChoice = "";
+            string[] computer = {"1" , "2" , "3"};
+
             void Rock()
             {
                 Console.WriteLine(@"
@@ -45,19 +50,16 @@ namespace rock_papper_sissors
             }
 
             // Function Calls
-            Scoreboard();
-            // Rock();
-            // Papper();
-            // Scissors();
-
+            Scoreboard(HumanScore, ComputerScore);
+         
 
 
             // Display Header for the Game
 
-            void Scoreboard()
+            void Scoreboard(int HumanScore, int ComputerScore)
             {
                 Console.WriteLine("-------------------------------");
-                Console.WriteLine("|  Player: 0    |   Computer: 0");
+                Console.WriteLine($"|  Player: {HumanScore}    |   Computer: {ComputerScore}");
                 Console.WriteLine("-------------------------------");
                 Console.WriteLine("What would you like to throw?");
                 Console.WriteLine("1) Rock");
@@ -66,12 +68,21 @@ namespace rock_papper_sissors
                 Console.WriteLine("-------------------------------");
             // Accept User input
                 string userChoice = Console.ReadLine();
-                Game(userChoice);
+                DisplayHumanChoice(userChoice);
+                Console.WriteLine("vs");
+                DisplayComputerChoice(ComputerChoice);
                 
             };
 
+                void GenerateComputerChoice()
+                {
+                    Random r =new Random();
+                    int genRan = r.Next(1,3);
+                    ComputerChoice = computer[genRan];
+                }
 
-                void Game(string userChoice)
+
+                void DisplayHumanChoice(string userChoice)
                 {
                     if(userChoice == "1")
                     {
@@ -85,6 +96,23 @@ namespace rock_papper_sissors
                     {
                         Scissors();
                     }
+
+                }
+                void DisplayComputerChoice(string ComputerChoice)
+                {
+                    if(ComputerChoice == "1")
+                    {
+                        Rock();
+                    }
+                    else if (ComputerChoice == "2")
+                    {
+                        Papper();
+                    }
+                    else
+                    {
+                        Scissors();
+                    }
+
                 }
 
 
